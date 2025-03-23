cc1eb70f2309ad86b31ef359a8cb2cfb37d3a08d
Mateo Herrera <mateohe091@gmail.com>
2025-03-23 11:24:03 -0300

    Entrega final 1

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeaBXdoPOB5Kf1WTCMRI7aw0FpXKWZOUI",
-  authDomain: "proyecto-inicial-b9160.firebaseapp.com",
-  projectId: "proyecto-inicial-b9160",
-  storageBucket: "proyecto-inicial-b9160.firebasestorage.app",
-  messagingSenderId: "288094456550",
-  appId: "1:288094456550:web:b4368321ffec97a0220927",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
